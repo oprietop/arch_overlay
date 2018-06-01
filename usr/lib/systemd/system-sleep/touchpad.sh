4ead14610a3f3b20a8b6b7b93a2b503850fd4fd9
Oscar Prieto <oprieto@externos.itnow.es>
2018-06-01 08:21:13 +0200

    fix touchpad.sh

--- usr/lib/systemd/system-sleep/touchpad.sh
+++ usr/lib/systemd/system-sleep/touchpad.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 case "$1" in
-	pre)	/sbin/modprobe -r hid_multitouch
-		exit 0
-		;;
-	post)	/sbin/modprobe hid_multitouch
-		exit 0
-		;;
- 	*)	exit 1
-		;;
+    pre)
+        /sbin/modprobe -r hid_multitouch
+        /sbin/modprobe -r hid_generic
+        /sbin/modprobe -r i2c_hid
+        /sbin/modprobe -r hid
+        exit 0
+        ;;
+    post)
+        /sbin/modprobe hid
+        /sbin/modprobe i2c_hid
+        /sbin/modprobe hid_generic
+        /sbin/modprobe hid_multitouch
+        exit 0
+        ;;
+    *)  exit 1
+        ;;
 esac
